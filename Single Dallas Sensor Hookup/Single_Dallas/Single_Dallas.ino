4bf63bf674dedcc6c877bcf953a67fc476b37fda
aburklund <aburklund@radford.edu>
2020-01-24 11:34:42 -0500

    added comments and changes to Dallas Sensor Code

--- Single Dallas Sensor Hookup/Single_Dallas/Single_Dallas.ino
+++ Single Dallas Sensor Hookup/Single_Dallas/Single_Dallas.ino
@@ -1,29 +1,27 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-//This Is The Arduino Pin that we are feeding the Data into
-#define ONE_WIRE_BUS 3 //Change if not using pin 3
+#define ONE_WIRE_BUS 3
 
 OneWire oneWire(ONE_WIRE_BUS);
 
 DallasTemperature sensors(&oneWire);
 int resT = 12;
+//////////////////////////////////////////////////////////////////////////
+// Change This to the address specific to your sensor
+DeviceAddress dallas01={0x28, 0x4B, 0x4A, 0x74, 0x06, 0x00, 0x00, 0xB0};
+//////////////////////////////////////////////////////////////////////////
 
-//Individual Device Address
-DeviceAddress dallas01={0x28, 0x50, 0x1D, 0x50, 0x06, 0x00, 0x00, 0xA9};
-
-//Create a time Variable
 unsigned long time;
 
-void setup(void) {
-  // put your setup code here, to run once:
+
+void setup() {
   Serial.begin(9600);
   sensors.begin();
   sensors.setResolution(dallas01, resT);
 }
 
-void loop(void) {
-  // put your main code here, to run repeatedly:
+void loop() {
   sensors.requestTemperatures();
   delay(3000);
   printTemperature(dallas01); Serial.println("\t");
